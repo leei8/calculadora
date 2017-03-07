a9830adaeef48af111b7cc698e7b08d474f84234
ikaslea <ikaslea@gela07_01>
2017-03-07 13:02:51 +0100

    Calculadora implementada

--- src/calculadora/Main.java
+++ src/calculadora/Main.java
@@ -17,8 +17,13 @@ import java.awt.Font;
 public class Main extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-
+	private JTextField pantalla;
+	private boolean decimal = false;
+	private double numero1;
+	private double numero2;
+	private String operador;
+	private double resultado;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -49,7 +54,7 @@ public class Main extends JFrame {
 		JButton button1 = new JButton("1");
 		button1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				pantalla.setText(pantalla.getText()+button1.getText());
 			}
 		});
 		button1.setBounds(10, 76, 41, 41);
@@ -58,7 +63,7 @@ public class Main extends JFrame {
 		JButton button2 = new JButton("2");
 		button2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				pantalla.setText(pantalla.getText()+button2.getText());
 			}
 		});
 		button2.setBounds(59, 76, 41, 41);
@@ -67,7 +72,7 @@ public class Main extends JFrame {
 		JButton button3 = new JButton("3");
 		button3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				pantalla.setText(pantalla.getText()+button3.getText());
 			}
 		});
 		button3.setBounds(108, 76, 41, 41);
@@ -76,7 +81,7 @@ public class Main extends JFrame {
 		JButton button4 = new JButton("4");
 		button4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				pantalla.setText(pantalla.getText()+button4.getText());
 			}
 		});
 		button4.setBounds(10, 126, 41, 41);
@@ -85,7 +90,7 @@ public class Main extends JFrame {
 		JButton button5 = new JButton("5");
 		button5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				pantalla.setText(pantalla.getText()+button5.getText());
 			}
 		});
 		button5.setBounds(59, 126, 41, 41);
@@ -94,7 +99,7 @@ public class Main extends JFrame {
 		JButton button6 = new JButton("6");
 		button6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				pantalla.setText(pantalla.getText()+button6.getText());
 			}
 		});
 		button6.setBounds(108, 126, 41, 41);
@@ -103,7 +108,7 @@ public class Main extends JFrame {
 		JButton button7 = new JButton("7");
 		button7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				pantalla.setText(pantalla.getText()+button7.getText());
 			}
 		});
 		button7.setBounds(10, 176, 41, 41);
@@ -112,7 +117,7 @@ public class Main extends JFrame {
 		JButton button8 = new JButton("8");
 		button8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				pantalla.setText(pantalla.getText()+button8.getText());
 			}
 		});
 		button8.setBounds(59, 176, 41, 41);
@@ -121,7 +126,7 @@ public class Main extends JFrame {
 		JButton button9 = new JButton("9");
 		button9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				pantalla.setText(pantalla.getText()+button9.getText());
 			}
 		});
 		button9.setBounds(108, 176, 41, 41);
@@ -130,7 +135,10 @@ public class Main extends JFrame {
 		JButton buttonMas = new JButton("+");
 		buttonMas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				decimal=false;
+				numero1 = Double.parseDouble(pantalla.getText());
+				operador = buttonMas.getText();
+				pantalla.setText(null);
 			}
 		});
 		buttonMas.setBounds(157, 76, 62, 39);
@@ -139,7 +147,10 @@ public class Main extends JFrame {
 		JButton buttonMinus = new JButton("-");
 		buttonMinus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				decimal=false;
+				numero1 = Double.parseDouble(pantalla.getText());
+				operador = buttonMinus.getText();
+				pantalla.setText(null);
 			}
 		});
 		buttonMinus.setBounds(157, 126, 62, 39);
@@ -148,7 +159,10 @@ public class Main extends JFrame {
 		JButton buttonEntre = new JButton("/");
 		buttonEntre.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				decimal=false;
+				numero1 = Double.parseDouble(pantalla.getText());
+				operador = buttonEntre.getText();
+				pantalla.setText(null);
 			}
 		});
 		buttonEntre.setBounds(157, 176, 62, 39);
@@ -157,7 +171,10 @@ public class Main extends JFrame {
 		JButton buttonPor = new JButton("*");
 		buttonPor.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				decimal=false;
+				numero1 = Double.parseDouble(pantalla.getText());
+				operador = buttonPor.getText();
+				pantalla.setText(null);
 			}
 		});
 		buttonPor.setBounds(157, 226, 62, 39);
@@ -166,7 +183,7 @@ public class Main extends JFrame {
 		JButton button0 = new JButton("0");
 		button0.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				pantalla.setText(pantalla.getText()+button0.getText());
 			}
 		});
 		button0.setBounds(10, 226, 41, 41);
@@ -175,7 +192,11 @@ public class Main extends JFrame {
 		JButton buttonComa = new JButton(".");
 		buttonComa.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				
+				if (decimal==false){
+					pantalla.setText(pantalla.getText()+buttonComa.getText());
+					decimal=true;
+				}
 			}
 		});
 		buttonComa.setBounds(59, 226, 41, 41);
@@ -184,19 +205,21 @@ public class Main extends JFrame {
 		JButton buttonC = new JButton("C");
 		buttonC.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				decimal=false;
+				pantalla.setText(null);
+				
 			}
 		});
 		buttonC.setBounds(108, 226, 42, 42);
 		contentPane.add(buttonC);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Gulim", Font.PLAIN, 11));
-		textField.setText("00.00");
-		textField.setHorizontalAlignment(SwingConstants.RIGHT);
-		textField.setBounds(10, 11, 207, 50);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		pantalla = new JTextField();
+		pantalla.setEditable(false);
+		pantalla.setFont(new Font("Gulim", Font.PLAIN, 11));
+		pantalla.setHorizontalAlignment(SwingConstants.RIGHT);
+		pantalla.setBounds(10, 11, 207, 50);
+		contentPane.add(pantalla);
+		pantalla.setColumns(10);
 		
 		JButton buttonPlusMinus = new JButton("+/-");
 		buttonPlusMinus.addActionListener(new ActionListener() {
@@ -210,7 +233,18 @@ public class Main extends JFrame {
 		JButton buttonResultado = new JButton("=");
 		buttonResultado.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//TODO
+				decimal=false;
+				numero2=Double.parseDouble(pantalla.getText());
+				if (operador.equals(buttonMas.getText())){
+					resultado = numero1 + numero2;
+					pantalla.setText(String.valueOf(resultado));
+				} else if (operador.equals(buttonMinus.getText())){
+					resultado = numero1 - numero2;
+				} else if (operador.equals(buttonEntre.getText())){
+					resultado = numero1 / numero2;
+				} else if (operador.equals(buttonPor.getText())){
+					resultado = numero1 * numero2;
+				}
 			}
 		});
 		buttonResultado.setBounds(118, 276, 101, 39);
